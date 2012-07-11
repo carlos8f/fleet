c8d56d219ea1f249f410ddbf88b3751b2d9007cf
Carlos Rodriguez <carlos@s8f.org>
2012-07-11 15:18:24 -0700

    Fix fleet reference in help

--- bin/help.sh
+++ bin/help.sh
@@ -6,5 +6,5 @@ if test -z "$1" || test "$1" = "commands"; then
 elif test -e "$rootdir/man1/$1.1"; then
     man "$rootdir/man1/$1.1"
 else 
-    echo "No manual entry for fleet $1"
+    echo "No manual entry for s8fleet $1"
 fi
